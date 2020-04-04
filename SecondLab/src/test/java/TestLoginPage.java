8eca1cdb5056cc511b5418f52f522dcfe709ffb6
Kiselev Arseniy <kis.ars@yandex.ru>
2020-04-05 00:18:55 +0300

    Added category tests

--- SecondLab/src/test/java/TestLoginPage.java
+++ SecondLab/src/test/java/TestLoginPage.java
@@ -1,18 +1,24 @@
+
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.junit.Test;
-import java.io.File;
+import org.junit.experimental.categories.Category;
+
 public class TestLoginPage {
     WebDriver driver = new ChromeDriver();
     Loginpage login = new Loginpage(driver);
     private String user = "test";
     private String password = "uniquepass";
+
     @Test
+    @Category(Categories.Logintests.class)
     public void TestLoginFunction()  {
         login.performLogin(user,password);
         driver.quit();
     }
+
     @Test
+    @Category(Categories.Logintests.class)
     public void TestLoginFailFunction()  {
         password = "12345467";
         login.performWrongLogin(user,password);
